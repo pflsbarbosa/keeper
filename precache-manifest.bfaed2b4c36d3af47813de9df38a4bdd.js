ad60285c32010bcb74ef6fb06882678b31fbeb70
Paulo Barbosa <pflsbarbosa@gmail.com>
2020-06-26 10:11:15 +0100

    Updates

---
+++ precache-manifest.bfaed2b4c36d3af47813de9df38a4bdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e34a3875115a3ad82c4753432961ff0",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "4a186ac8d27bdf6534f7",
+    "url": "/keeper/static/js/2.7d562a81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper/static/js/2.7d562a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b0813b30b79a684ee66",
+    "url": "/keeper/static/js/main.2730a7d0.chunk.js"
+  },
+  {
+    "revision": "a0fe8f1cafe3e4405f4e",
+    "url": "/keeper/static/js/runtime-main.534cd8d7.js"
+  }
+]);
