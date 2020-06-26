430ac7bea265703c1099e950f57a503a219a66c4
Paulo Barbosa <pflsbarbosa@gmail.com>
2020-06-26 10:08:38 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CreateIcon from '@material-ui/icons/Create';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <CreateIcon /> keeper 
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
